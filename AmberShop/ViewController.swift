bf1ae6f40d4e502c7bf17fd23371d4b5a13012ee
Kirill <kirill@ksyste.ms>
2021-11-20 20:07:52 +0200

    Initial Commit

---
+++ AmberShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmberShop
+//
+//  Created by Kyrylo Chernov on 20.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
