53e830ecf5f14d69ecbd4e48ec9b5ddababb3dd8
Kirill <kirill@ksyste.ms>
2021-11-29 16:32:53 +0200

    UI fixes

--- AmberShop/Cells/Cart/CartTableViewCell.swift
+++ AmberShop/Cells/Cart/CartTableViewCell.swift
@@ -12,7 +12,8 @@ class CartTableViewCell: UITableViewCell {
     @IBOutlet weak var productsTableView: AutoSizingUITableView!
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var totalPriceLabel: UILabel!
-    @IBOutlet weak var tableViewHeight: NSLayoutConstraint!
+    
+    var dataRows = ["test","test1","test2"]
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -41,18 +42,23 @@ class CartTableViewCell: UITableViewCell {
 
 extension CartTableViewCell: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        3
+        dataRows.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCartTableViewCell", for: indexPath) as! ProductCartTableViewCell
         cell.iconImageVIew.image = UIImage(named: "testImg")
-        cell.titleLabel.text = "TEST"
+        cell.titleLabel.text = dataRows[indexPath.row]
+        cell.deleteAction = { [weak self] in
+            guard let self = self else {return}
+            self.dataRows.remove(at: indexPath.row)
+            self.productsTableView.reloadData()
+        }
         return cell
         
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         UITableView.automaticDimension
     }
-  
 }
--- AmberShop/Cells/Cart/ProductCartTableViewCell.swift
+++ AmberShop/Cells/Cart/ProductCartTableViewCell.swift
@@ -9,13 +9,25 @@ import UIKit
 
 class ProductCartTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var iconImageVIew: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var containerStackView: UIStackView!
     @IBOutlet weak var priceLabel: UILabel!
-    @IBOutlet weak var decrementButton: UIButton!
-    @IBOutlet weak var incrementButton: UIButton!
     @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    var count = 1
+    public var deleteAction: (() -> Void) = {}
+    
+    @IBAction func decrementAcction(_ sender: Any) {
+        count = count - 1
+        countLabel.text = count.description
+    }
+    @IBAction func incrementAction(_ sender: Any) {
+        count = count + 1
+        countLabel.text = count.description
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -24,8 +36,19 @@ class ProductCartTableViewCell: UITableViewCell {
         
         iconImageVIew.layer.borderWidth = 0.5
         iconImageVIew.layer.borderColor = UIColor.black.withAlphaComponent(0.3).cgColor
+        
+        containerStackView.backgroundColor = UIColor.hexColor(hex: "F7F7F7")
+        containerStackView.layer.cornerRadius = 10
+        
+        containerView.layer.borderWidth = 0.5
+        containerView.layer.borderColor = UIColor.black.withAlphaComponent(0.3).cgColor
+        
+        deleteButton.addTarget(self, action: #selector(deleteButtonDidClick), for: .touchUpInside)
     }
 
+    @objc func deleteButtonDidClick() {
+        deleteAction()
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
